13a90c10b91d5a354a69f278839e2f8671ea310f
TharushaGonawala <157585076+TharushaJM@users.noreply.github.com>
2025-12-17 03:19:29 +0530

    First Commit

---
+++ src/pharmacy/managment/system/PharmacyManagmentSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pharmacy.managment.system;
+
+/**
+ *
+ * @author Tharusha
+ */
+public class PharmacyManagmentSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
